5a9e25f3fac09102a65bb3c4cae620d1a4886bff
imarkov <imarkov@tarantool.org>
2018-01-30 15:52:07 +0300

    security: Introduce entities
    Entity is new level of abstraction in Access control lists.
    
    Entity unites group of objects. E.g., entity of spaces includes
    all spaces. So the picture of abstraction now looks like:
    | - - - - -> functions entity -> functions
    universe- -> spaces entity -> spaces
    | - - - - -> sequences entity -> sequence
    (arrow here means relation "includes")
    
    So, now it's available to execute such grants:
    box.schema.user.grant("guest", "read", "space")
    
    After that user 'guest' will get read access to all
    non-system spaces.
    
    Privileges to high level structures are not shared with actual
    lower level privileges. So revoking in highlevel does not revoke
    privilege in lower level.
    
    This patch is inspired by introducing Create privilege, which can not
    be applied to specific object but to class of objects.
    
    Prerequisite #945

--- src/box/lua/schema.lua
+++ src/box/lua/schema.lua
@@ -1710,6 +1710,9 @@ local function object_resolve(object_type, object_name)
         return 0
     end
     if object_type == 'space' then
+        if object_name == nil or object_name == 0 then
+            return 0
+        end
         local space = box.space[object_name]
         if  space == nil then
             box.error(box.error.NO_SUCH_SPACE, object_name)
@@ -1717,6 +1720,9 @@ local function object_resolve(object_type, object_name)
         return space.id
     end
     if object_type == 'function' then
+        if object_name == nil or object_name == 0 then
+            return 0
+        end
         local _func = box.space[box.schema.FUNC_ID]
         local func
         if type(object_name) == 'string' then
@@ -1731,6 +1737,9 @@ local function object_resolve(object_type, object_name)
         end
     end
     if object_type == 'sequence' then
+        if object_name == nil or object_name == 0 then
+            return 0
+        end
         local seq = sequence_resolve(object_name)
         if seq == nil then
             box.error(box.error.NO_SUCH_SEQUENCE, object_name)
@@ -1738,6 +1747,9 @@ local function object_resolve(object_type, object_name)
         return seq
     end
     if object_type == 'role' then
+        if object_name == nil or object_name == 0 then
+            return 0
+        end
         local _user = box.space[box.schema.USER_ID]
         local role
         if type(object_name) == 'string' then
